0b2501fe917c012c9dcc9f5bb8084915e92c3c58
PaSeRouS <paser96@mail.ru>
2022-06-14 15:40:24 +0300

    Exclude redudant 'if'

--- property/migrations/0004_auto_20220611_1522.py
+++ property/migrations/0004_auto_20220611_1522.py
@@ -7,9 +7,8 @@ def set_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
 
     for flat in Flat.objects.all():
-        if flat.construction_year >= 2015:
-            flat.new_building = True
-            flat.save()
+        flat.new_building = flat.construction_year >= 2015
+        flat.save()
             
 
 class Migration(migrations.Migration):
